8e05ea267eaa01d9e34f4c2726f708623709b4a0
Nilu Isakova <niluisakova@gmail.com>
2024-07-23 03:30:40 -0500

    OOP hwm1

--- src/BankingSystem/Account.java
+++ src/BankingSystem/Account.java
@@ -3,4 +3,10 @@ package BankingSystem;
 public class Account {
     private String accountNumber;
     private double balance;
-}
+
+    // Constructor
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
